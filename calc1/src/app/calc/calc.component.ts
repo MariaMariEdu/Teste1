c26e8a3d1252a7e178ef78bddf7dfc3a02fafd9a
Maria Eduarda do Carmo Brito <maric.brito15@gmail.com>
2025-03-21 10:26:50 -0300

    blindings feitas

--- calc1/src/app/calc/calc.component.ts
+++ calc1/src/app/calc/calc.component.ts
@@ -7,5 +7,59 @@ import { Component } from '@angular/core';
   styleUrl: './calc.component.css'
 })
 export class CalcComponent {
+ num1: number = 0;
+  num2: number = 0;
+  result: number | null = null;
+
+  operator: string = "#";
+num2Visible: any;
+sun: any;
+
+  sum() {
+    this.result = this.num1 + this.num2;
+    this.operator = '+';
+  }
+
+  sub() {
+    this.result = this.num1 - this.num2;
+    this.operator = '-';
+  }
+
+  mult() {
+    this.result = this.num1 * this.num2;
+    this.operator = '*';
+  }
+
+  div() {
+    if (this.num2 !== 0) {
+      this.result = this.num1 / this.num2;
+      this.operator = '÷';
+    } else {
+      this.result = null;
+      alert('É impossivel dividir o zero! (* ￣︿￣)');
+    }
+  }
+
+  pow() {
+    this.result = Math.pow(this.num1, this.num2);
+    this.operator = 'xⁿ';
+  }
+
+  sqrt() {
+    if (this.num1 >= 0) {
+      this.result = Math.sqrt(this.num1);
+      this.operator = '√';
+    } else {
+      alert('É impossivel calcular raiz de número negativoo! ヽ（≧□≦）ノ ');
+      this.result = null;
+    }
+  }
+
+  clear() {
+    this.num1 = 0;
+    this.num2 = 0;
+    this.result = null;
+    this.operator = '';
+  }
 
 }
